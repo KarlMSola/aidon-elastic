aef998a0fa0c4a0b0a814f9d5e69344f18822d7a
Karl Martin Sola <kms@shuttle.localdomain>
2020-06-14 20:51:20 +0200

    Update to 7.7.1 and bugfix

--- aidon_elastic.py
+++ aidon_elastic.py
@@ -1,60 +1,65 @@
-#!/usr/bin/pythonr
+#!/usr/bin/python
 
 import serial, time, sys, json
 from aidon_obis import *
+from elasticsearch.client.ingest import IngestClient
 from elasticsearch import Elasticsearch
 
-PUT _ingest/pipeline/timestamp
-{
-  "description": "Adds timestamp called timestamp to documents",
-  "processors": [
-    {
-      "set": {
-      "field": "_source.timestamp",
-      "value": "{{_ingest.timestamp}}"
+# Define variables
+es_index='aidon-2020'
+es_server='elastic1'
+es_port=9200
+
+def create_pipeline():
+  pipeline_body = {
+    "description": "Adds timestamp called timestamp to documents",
+    "processors": [
+      {
+        "set": {
+        "field": "_source.timestamp",
+        "value": "{{_ingest.timestamp}}"
+        }
+      }
+    ]
+  }
+  p = IngestClient(es)
+  p.put_pipeline(id='timestamp', body=pipeline_body )
+
+def create_index():
+  index_settings = {
+    "settings" : {
+      "index" : {
+        "number_of_shards" : "1",
+        "default_pipeline" : "timestamp",
+        "number_of_replicas" : "0"
       }
     }
-  ]
-}
-
-settings = {
-  "settings" : {
-    "index" : {
-      "number_of_shards" : "1",
-      "default_pipeline" : "timestamp",
-      "number_of_replicas" : "0"
-    }
   }
-}
-
-response = elastic_client.indices.create(
-    index="some_new_index",
-    body=mapping,
-    ignore=400 # ignore 400 already exists code
-)
-
-if 'acknowledged' in response:
+  response = es.indices.create(
+      index=es_index,
+      body=index_settings,
+      ignore=400 # ignore 400 already exists code
+  )
+  if 'acknowledged' in response:
     if response['acknowledged'] == True:
-        print ("INDEX MAPPING SUCCESS FOR INDEX:", response['index'])
-
-# catch API error response
-elif 'error' in response:
+      print ("INDEX MAPPING SUCCESS FOR INDEX:", response['index'])
+  # catch API error response
+  elif 'error' in response:
     print ("ERROR:", response['error']['root_cause'])
     print ("TYPE:", response['error']['type'])
+  # print out the response:
+  print ('\nresponse:', response)
 
-# print out the response:
-print ('\nresponse:', response)
-
-
+def aidon_callback(fields):
+    res = es.index(index=es_index,doc_type='_doc',body=json.dumps(fields))
 
 if len (sys.argv) != 2:
 	print "Usage: ... <serial_port>"
 	sys.exit(0)
 
-es=Elasticsearch([{'host':'elastic1','port':9200}])
-
-def aidon_callback(fields):
-    res = es.index(index='aidon-2020',doc_type='_doc',body=json.dumps(fields))
+es=Elasticsearch([{'host':es_server,'port':es_port}])
+create_pipeline()
+create_index()
 
 ser = serial.Serial(sys.argv[1], 2400, timeout=0.05, parity=serial.PARITY_NONE)
 a = aidon(aidon_callback)
@@ -63,4 +68,3 @@ while(1):
 	while ser.inWaiting():
 		a.decode(ser.read(1))
 	time.sleep(0.01)
-
